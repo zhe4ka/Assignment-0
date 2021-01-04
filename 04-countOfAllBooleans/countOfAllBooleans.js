939c28eb9c04fcd26d2b4392e425ade542df402b
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2021-01-04 01:24:44 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,10 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  count=0;
+  arr.forEach(value => {
+    if (typeof value=='boolean' )
+      count++;
+  });
+  return count;
 }
 
 // Do not edit this line;
