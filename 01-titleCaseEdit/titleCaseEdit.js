ea34c14be65a2cd87dd43821378907c967cbcf84
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2021-01-04 01:22:33 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,9 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+   title = title.toLowerCase().split(' ');
+  for (var i = 0; i < title.length; i++) {
+    title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1); 
+  }
+  return title.join(' ');
 }
 
 // Do not edit this line;
