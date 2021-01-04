38dd303fc84f861cc0449ea4829015560ed4ff4d
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2021-01-04 01:27:48 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,10 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  count = 0;
+for(var i = 0; i < nums.length; i++) {
+  if(nums[i]%2==0 && nums[i]!=0)
+  count++;
+  }
+return count;
 }
 
 // Do not edit this line;
