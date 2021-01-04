1891959960f5f1a3e173079cdec39a681fe5f9cd
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2021-01-04 01:31:50 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,21 @@
 function isPalindrome(word) {
-  // Insert code here;
+  var stack=new Array() ;
+ var queue=new Array();
+ var palindromeOrNot=true;
+ 
+  for (var i=0; i<word.length; i++){
+word[i] = word[i].toLowerCase();
+stack.push(word[i]);
+queue.push(word[i]);
+}
+while(queue.length>0){
+  var queueVar=queue.shift();
+  var stackVar=stack.pop();
+}
+if(queueVar!=stackVar){
+   palindromeOrNot=false; 
+}
+ return palindromeOrNot;
 }
 
 // Do not edit this line;
