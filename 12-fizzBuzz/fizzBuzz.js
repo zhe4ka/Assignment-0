9d350773fcd863e96e082ed9a6602465f7770ed4
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2021-01-04 01:30:56 -0500

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,18 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+var nums=[] ;
+ j=0;
+  for (var i=start; i<=end; i++){
+    if (i%5==0 && i%3==0)
+    nums[j]="FizzBuzz;
+    else if (i%3==0 && i%5!=0)
+    nums[j]="Fizz";
+    else if (i%5==0 && i%3==0)
+    nums[j]="Buzz";
+    else
+     nums[j]=i;
+    j++;
+  }
+ return nums;
 }
 
 // Do not edit this line;
